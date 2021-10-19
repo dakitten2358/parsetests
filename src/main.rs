81e6672419aabc2dff2f852389d032189211d1cb
jason-william.murray <jay.w.murray@gmail.com>
2021-10-19 12:51:17 -0400

    more work on parsing/colored output

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use serde::{Deserialize};
 use std::fs::File;
 use std::io::prelude::*;
+use colored::*;
+
+#[derive(Debug, Deserialize)]
+enum EntryType {
+    Info,
+	Warning,
+	Error,
+}
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct Event {
-    r#type: String,
+    r#type: EntryType,
     message: String,
     context: String,
     artifact: String,
@@ -87,11 +95,58 @@ fn utf_from_bytes(buffer: &[u8]) -> String {
 }
 
 fn main() {
-    let mut file = File::open("index.json").expect("failed to open file");
+    let mut file = File::open("index2.json").expect("failed to open file");
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).expect("failed to read file");
     let index_json_string = utf_from_bytes(&buffer);
     let index_json = index_json_string.as_str();
-    let test_pass : TestPass = serde_json::from_str(index_json).expect("invalid json");
-    println!("{:?}", test_pass);
+    let mut test_pass : TestPass = serde_json::from_str(index_json).expect("invalid json");
+    test_pass.tests.sort_by(|a, b| a.full_test_path.cmp(&b.full_test_path));
+
+    let pass_message = "     Success ".bright_green();
+    let fail_message = "        Fail ".red();
+    let warn_message = "     Warning ".yellow();
+    let empty_spacer = "             ";
+
+    let log_info = "        Info ".white();
+    let log_warn = "     Warning ".yellow();
+    let log_error ="       Error ".red();
+
+    for test in test_pass.tests {
+        match test.state {
+            
+            TestResult::Success => println!("{}{}", pass_message, test.full_test_path.white()),
+            TestResult::Fail => 
+            {
+                println!("{}{}", fail_message, test.full_test_path.white());
+                for entry in test.entries {
+                    match entry.event.r#type {
+                        EntryType::Info => println!("{}{}{}", empty_spacer, log_info, entry.event.message),
+                        EntryType::Warning => {
+                            println!("{}{}{}", empty_spacer, log_warn, entry.event.message);
+                            println!("{}{}{}:{}", empty_spacer, empty_spacer, entry.filename, entry.line_number);
+                        },
+                        EntryType::Error => {
+                            println!("{}{}{}", empty_spacer, log_error, entry.event.message);
+                            println!("{}{}{}:{}", empty_spacer, empty_spacer, entry.filename, entry.line_number);
+                        }
+                    }
+                }
+            },
+            _ => println!("{}{}", warn_message, test.full_test_path.yellow()),
+        }
+        
+    }
+
+    if test_pass.failed > 0 {
+        println!("{}", format!("{} passed, {} failed, {} other", test_pass.succeeded, test_pass.failed, test_pass.not_run + test_pass.succeeded_with_warnings).red());
+    }
+    else if test_pass.not_run > 0 || test_pass.succeeded_with_warnings > 0 {
+        println!("{}", format!("{} passed, {} failed, {} other", test_pass.succeeded, test_pass.failed, test_pass.not_run + test_pass.succeeded_with_warnings).yellow());
+    }
+    else
+    {
+        println!("{}", format!("{} passed, {} failed, {} other", test_pass.succeeded, test_pass.failed, test_pass.not_run + test_pass.succeeded_with_warnings).bright_green());
+    }
+    println!("{}s elapsed", test_pass.total_duration);
 }
