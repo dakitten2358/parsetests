01aa47276aa55096516b5483f639aed997fa66f2
jason-william.murray <jay.w.murray@gmail.com>
2021-10-19 09:51:51 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,97 @@
+use serde::{Deserialize};
+use std::fs::File;
+use std::io::prelude::*;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Event {
+    r#type: String,
+    message: String,
+    context: String,
+    artifact: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Entry {
+    event: Event,
+    filename: String,
+    line_number: i32,
+    timestamp: String,
+}
+
+#[derive(Debug, Deserialize)]
+enum TestResult {
+    NotRun,					// Automation test was not run
+	InProcess,				// Automation test is running now
+	Fail,					// Automation test was run and failed
+	Success,				// Automation test was run and succeeded
+	NotEnoughParticipants,	// Automation test was not run due to number of participan
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Test {
+    test_display_name: String,
+    full_test_path: String,
+    state: TestResult,
+    entries: Vec<Entry>,
+    warnings: i32,
+    errors: i32,
+    artifacts: Vec<String>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Device {
+    device_name: String,
+    instance: String,
+    platform: String,
+    #[serde(rename="oSVersion")]
+    os_version: String,
+    model: String,
+    #[serde(rename="gPU")]
+    gpu: String,
+    #[serde(rename="cPUModel")]
+    cpu_model: String,
+    #[serde(rename="rAMInGB")]
+    ram_in_gb: i32,
+    render_mode: String,
+    #[serde(rename="rHI")]
+    rhi: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct TestPass {
+    devices: Option<Vec<Device>>,
+    report_created_on: String,
+    succeeded: i32,
+    succeeded_with_warnings: i32,
+    failed: i32,
+    not_run: i32,
+    in_process: Option<i32>,
+    total_duration: f32,
+    comparison_exported: bool,
+    comparison_export_directory: String,
+    tests: Vec<Test>,
+}
+
+fn utf_from_bytes(buffer: &[u8]) -> String {
+    let index_json_string = String::from_utf8_lossy(&buffer).into_owned();
+    return if index_json_string.starts_with("\u{feff}") {
+        String::from_utf8_lossy(&buffer[3..]).into_owned()
+    } else {
+        index_json_string
+    };
+}
+
+fn main() {
+    let mut file = File::open("index.json").expect("failed to open file");
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).expect("failed to read file");
+    let index_json_string = utf_from_bytes(&buffer);
+    let index_json = index_json_string.as_str();
+    let test_pass : TestPass = serde_json::from_str(index_json).expect("invalid json");
+    println!("{:?}", test_pass);
+}
