ff3c35bb36b3f95f222db0b4c49b21a41cde82c4
jason-william.murray <jay.w.murray@gmail.com>
2021-10-20 09:16:44 -0400

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-use serde::{Deserialize};
+use colored::*;
+use serde::Deserialize;
 use std::fs::File;
 use std::io::prelude::*;
-use colored::*;
 use std::process::Command;
 
 #[derive(Debug, Deserialize)]
 enum EntryType {
     Info,
-	Warning,
-	Error,
+    Warning,
+    Error,
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 struct Event {
-    #[serde(rename="type")]
+    #[serde(rename = "type")]
     entry_type: EntryType,
     message: String,
     context: String,
@@ -32,11 +32,11 @@ struct Entry {
 
 #[derive(Debug, Deserialize)]
 enum TestResult {
-    NotRun,					// Automation test was not run
-	InProcess,				// Automation test is running now
-	Fail,					// Automation test was run and failed
-	Success,				// Automation test was run and succeeded
-	NotEnoughParticipants,	// Automation test was not run due to number of participan
+    NotRun,                // Automation test was not run
+    InProcess,             // Automation test is running now
+    Fail,                  // Automation test was run and failed
+    Success,               // Automation test was run and succeeded
+    NotEnoughParticipants, // Automation test was not run due to number of participan
 }
 
 #[derive(Debug, Deserialize)]
@@ -57,17 +57,17 @@ struct Device {
     device_name: String,
     instance: String,
     platform: String,
-    #[serde(rename="oSVersion")]
+    #[serde(rename = "oSVersion")]
     os_version: String,
     model: String,
-    #[serde(rename="gPU")]
+    #[serde(rename = "gPU")]
     gpu: String,
-    #[serde(rename="cPUModel")]
+    #[serde(rename = "cPUModel")]
     cpu_model: String,
-    #[serde(rename="rAMInGB")]
+    #[serde(rename = "rAMInGB")]
     ram_in_gb: i32,
     render_mode: String,
-    #[serde(rename="rHI")]
+    #[serde(rename = "rHI")]
     rhi: String,
 }
 
@@ -104,8 +104,7 @@ fn load_file(file_path: &str) -> String {
 }
 
 #[derive(Debug, Deserialize)]
-struct TestConfiguration
-{
+struct TestConfiguration {
     path_to_unrealengine: String,
     path_to_project: String,
     path_to_reports: String,
@@ -113,7 +112,7 @@ struct TestConfiguration
 
 fn main() {
     let config_toml = load_file("testconfig.toml");
-    let config : TestConfiguration = toml::from_str(config_toml.as_str()).expect("failed to parse toml");
+    let config: TestConfiguration = toml::from_str(config_toml.as_str()).expect("failed to parse toml");
 
     println!("starting process");
     let mut run_test_command = Command::new(config.path_to_unrealengine)
@@ -138,7 +137,7 @@ fn main() {
 
     let index_json_string = load_file(format!("{}\\index.json", config.path_to_reports).as_str());
     let index_json = index_json_string.as_str();
-    let mut test_pass : TestPass = serde_json::from_str(index_json).expect("invalid json");
+    let mut test_pass: TestPass = serde_json::from_str(index_json).expect("invalid json");
     test_pass.tests.sort_by(|a, b| a.full_test_path.cmp(&b.full_test_path));
 
     let pass_message = "     Success ".bright_green();
@@ -148,7 +147,7 @@ fn main() {
 
     let log_info = "        Info ".white();
     let log_warn = "     Warning ".yellow();
-    let log_error ="       Error ".red();
+    let log_error = "       Error ".red();
 
     for test in test_pass.tests {
         match test.state {
@@ -161,17 +160,16 @@ fn main() {
                         EntryType::Warning => {
                             println!("{}{}{}", empty_spacer, log_warn, entry.event.message);
                             println!("{}{}{}:{}", empty_spacer, empty_spacer, entry.filename, entry.line_number);
-                        },
+                        }
                         EntryType::Error => {
                             println!("{}{}{}", empty_spacer, log_error, entry.event.message);
                             println!("{}{}{}:{}", empty_spacer, empty_spacer, entry.filename, entry.line_number);
                         }
                     }
                 }
-            },
+            }
             _ => println!("{}{}", warn_message, test.full_test_path.yellow()),
         }
-        
     }
 
     let succeeded_count = test_pass.succeeded;
@@ -180,11 +178,9 @@ fn main() {
 
     if test_pass.failed > 0 {
         println!("{}", format!("{} passed, {} failed, {} other", succeeded_count, failed_count, other_count).red());
-    }
-    else if test_pass.not_run > 0 || test_pass.succeeded_with_warnings > 0 {
+    } else if test_pass.not_run > 0 || test_pass.succeeded_with_warnings > 0 {
         println!("{}", format!("{} passed, {} failed, {} other", succeeded_count, failed_count, other_count).yellow());
-    }
-    else {
+    } else {
         println!("{}", format!("{} passed, {} failed, {} other", succeeded_count, failed_count, other_count).bright_green());
     }
     println!("{}s elapsed", test_pass.total_duration);
